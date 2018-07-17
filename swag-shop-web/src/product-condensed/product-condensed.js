722110b0795e6c61d3fa973b720eb851d24f9ff7
techemayo <muhammadasifmayo@gmail.com>
2018-07-17 22:07:37 +0500

    Remove item button in the wish list activated - Project complete

--- swag-shop-web/src/product-condensed/product-condensed.js
+++ swag-shop-web/src/product-condensed/product-condensed.js
@@ -1,11 +1,22 @@
 import React, {Component} from 'react';
 import './product-condensed.css';
-
+import DataService from "../services/data-service";
+var ds = new DataService();
 class ProductCondensed extends Component{
+    
+    constructor(props){
+        super(props);
+        //Bind
+        this.removeProduct = this.removeProduct.bind(this);
+    }
+    removeProduct =()=>{
+        ds.removeWishListItem(this.props.product);
+    }
+    
     render(){
         return(
             <li className="list-group-item pc-condensed">
-                <a className="btn btn-outline-danger">X</a>
+                <a className="btn btn-outline-danger" onClick={()=>this.removeProduct()}>X</a>
                 <p>{this.props.product.title} | $ {this.props.product.price}</p>
             </li>
         );
