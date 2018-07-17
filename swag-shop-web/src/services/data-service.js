21ea06d79a5f45b5c738456218b79e143ee6580b
techemayo <muhammadasifmayo@gmail.com>
2018-07-17 19:28:55 +0500

    singleton data service added for wishlist

---
+++ swag-shop-web/src/services/data-service.js
@@ -0,0 +1,24 @@
+let instance = null;
+var wishList =[];
+class DataService{
+    constructor(){
+        if(!instance){
+            instance=this;
+        }
+        return instance;
+    }
+    
+    addWishListItem= item =>{
+        wishList.push(item);
+    }
+    removeWishListItem = item =>{
+        for(var x=0; x<wishList.length;x++){
+            if(wishList[x]._id===item._id){
+                wishList.splice(x,1);
+                break;
+            }
+        }
+    }
+}
+
+export default DataService;
