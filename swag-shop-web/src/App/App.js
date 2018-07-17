3473353f7097df10445e07120c0e981e567354ed
techemayo <muhammadasifmayo@gmail.com>
2018-07-17 13:01:43 +0500

    state modified for products and was mapped

--- swag-shop-web/src/App/App.js
+++ swag-shop-web/src/App/App.js
@@ -7,19 +7,31 @@ const http = new HttpService();
 class App extends Component {
     constructor(props){
         super(props);
+        
+        this.state={products:[]};
         //Bind Functions
         this.loadData = this.loadData.bind(this);
+        this.productList = this.productList.bind(this);
         this.loadData();
     }
     
     loadData =() => {
-        http.getProducts().then(products =>{
-            console.log(products);
+        var self = this;
+        http.getProducts().then(data =>{
+            self.setState({products:data})
         },err =>{
             
         });
     }
     
+    productList =()=>{
+        const list = this.state.products.map((product) => 
+        <div className="col-sm-4" key={product._id}>
+            <Product title={product.title} price={product.price} imgUrl={product.imgUrl} />
+        </div>  
+    );
+    return(list);
+}
   render() {
     return (
       <div className="App">
@@ -29,9 +41,7 @@ class App extends Component {
         </header>
         <div className="container App-main">
         <div className="row">
-         <Product className="col-sm-4" price="4.23" title="Cool Toy Gun" imgUrl = "http://americanlookout.com/wp-content/uploads/2017/05/54ca62c3d99f0_-_waterguns-5.jpg"/>
-        <Product className="col-sm-4" price="4.23" title="Cool Toy Gun" imgUrl = "http://americanlookout.com/wp-content/uploads/2017/05/54ca62c3d99f0_-_waterguns-5.jpg"/>
-        <Product className="col-sm-4" price="4.23" title="Cool Toy Gun" imgUrl = "http://americanlookout.com/wp-content/uploads/2017/05/54ca62c3d99f0_-_waterguns-5.jpg"/>
+      {this.productList()}
         </div>
          
         </div>
