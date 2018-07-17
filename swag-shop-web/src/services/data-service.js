f40d13847ada157b541b1cd7c71a9f3a808d12d2
techemayo <muhammadasifmayo@gmail.com>
2018-07-17 19:49:34 +0500

    Notification posting integrated

--- swag-shop-web/src/services/data-service.js
+++ swag-shop-web/src/services/data-service.js
@@ -1,3 +1,5 @@
+import NotificationService, {NOTIF_WISHLIST_CHANGED} from "./notification-service";
+ls = new NotificationService();
 let instance = null;
 var wishList =[];
 class DataService{
@@ -10,11 +12,13 @@ class DataService{
     
     addWishListItem= item =>{
         wishList.push(item);
+        ns.postNotification(NOTIF_WISHLIST_CHANGED,wishList);
     }
     removeWishListItem = item =>{
         for(var x=0; x<wishList.length;x++){
             if(wishList[x]._id===item._id){
                 wishList.splice(x,1);
+                ns.postNotification(NOTIF_WISHLIST_CHANGED,wishList);
                 break;
             }
         }
